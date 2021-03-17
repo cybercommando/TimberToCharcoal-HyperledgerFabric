e08afc4343d15735dc0d45ff0c4b742d8f59f7ba
root <root@kali.local>
2021-03-17 15:02:30 -0400

    Configuration of Chaincode - Underdevelopment

---
+++ chaincode/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const InvoiceContract = require('./lib/invoice-contract');
+
+module.exports.InvoiceContract = InvoiceContract;
+module.exports.contracts = [ InvoiceContract ];
