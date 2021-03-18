0d952b32d737d1f2dd680922ec2b69bef72a03eb
root <root@kali.local>
2021-03-18 18:46:17 -0400

    Configuration of Chaincode - configured with API sample endpoints

--- network.sh
+++ network.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 if [ "$1" == "start" ]; then
     echo "*********************************"
-    echo "      Generating artifacts       "
+    echo "      Generating Artifacts       "
     echo "*********************************"
     ./charcoal-network/scripts/generate.sh
     echo "*********************************"
-    echo "      Starting network           "
+    echo "      Starting Network           "
     echo "*********************************"
     ./charcoal-network/scripts/start.sh
     echo "*********************************"
     echo "      Installing Chaincode       "
     echo "*********************************"
     ./charcoal-network/scripts/install-cc.sh
+    echo "*********************************"
+    echo "      Registering Users          "
+    echo "*********************************"
+    ./charcoal-network/scripts/register-users.sh
 elif [ "$1" == "stop" ]; then
     ./charcoal-network/scripts/stop.sh
 elif [ "$1" == "install" ]; then
