e6a932f8c1d1d470c8668bc0a0064e93176f1941
root <root@kali.local>
2021-06-28 21:36:52 -0400

    Chaincode Implementation - get company status history and code improvements

---
+++ chaincode/lib/Models/Certifier.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class Certifier {
+    constructor() {
+        this.certifierId = '';
+        this.name = '';
+    }
+
+    static from(bufferOrJson) {
+        if (Buffer.isBuffer(bufferOrJson)) {
+            if (!bufferOrJson.length) {
+                return;
+            }
+            bufferOrJson = JSON.parse(bufferOrJson.toString('utf-8'));
+
+        }
+        return Object.assign(new Certifier(), bufferOrJson);
+    }
+
+    toBuffer() {
+        return Buffer.from(JSON.stringify(this));
+    }
+}
+
+module.exports = { Certifier };
